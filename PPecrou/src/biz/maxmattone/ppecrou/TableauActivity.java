6d2147caa93f872c8d5fa61c38891cfc27b0173f
maxmattone <mattonem@gmail.com>
2014-01-23 17:14:52 +0100

    FirstCommit

---
+++ PPecrou/src/biz/maxmattone/ppecrou/TableauActivity.java
@@ -0,0 +1,22 @@
+package biz.maxmattone.ppecrou;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TableauActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tableau);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.tableau, menu);
+		return true;
+	}
+
+}
